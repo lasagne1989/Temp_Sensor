387d96dc6ce0df84405adde9a5a0ffd86c6f7ba2
lasagne1989 <101409249+lasagne1989@users.noreply.github.com>
2023-03-03 10:26:28 +0000

    added time stamps

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import asyncio
 from connection import connection
 from temp_check import temp_check
-from datetime import datetime
+from time import time
 import on_off
 
 upper_limit = 18
@@ -12,15 +12,17 @@ async def main():
     heater = "start"
     while True:
         temp = await temp_check(sensors)
-        if temp > upper_limit and heater != "on":
-            await on_off.turn_off(plugs)
-            heater = "on"
-            print("Heater ON!")
-        if temp < lower_limit and heater != "off":
+        if temp < lower_limit and heater != "on":
             await on_off.turn_on(plugs)
+            heater = "on"
+            print("Heater on")
+            start_time = time()
+        if temp > upper_limit and heater != "off":
+            await on_off.turn_off(plugs)
             heater = "off"
-            print("Heater OFF!")
-        await asyncio.sleep(600)
+            end_time = time() - start_time
+            print(f"Heater off after {end_time/60} mins")
+        await asyncio.sleep(300)
 
 
 if __name__ == '__main__':
